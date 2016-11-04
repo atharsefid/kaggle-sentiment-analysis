82126af828cab033f3c05c7624f1aa7438d3abcd
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-11-04 16:49:22 -0400

    tune-params.py: verbosity

--- tune-params.py
+++ tune-params.py
@@ -5,7 +5,6 @@ Find the best estimator
 
 import argparse
 import math
-import time
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import GridSearchCV
@@ -16,7 +15,6 @@ import sentiment_analysis.data as data
 
 
 def main(args):
-    t1 = time.clock()
     X, y = data.load_training_data(args.train)
     #print(X)
     #print(y)
@@ -29,16 +27,14 @@ def main(args):
         'svc__C': [math.pow(10, power) for power in range(-4, 5)]
     }
 
-    gs = GridSearchCV(estimator=pipeline, param_grid=param_grid, cv=5, verbose=1).fit(X, y)
+    gs = GridSearchCV(estimator=pipeline, param_grid=param_grid, cv=5, verbose=args.verbose).fit(X, y)
     print("best params = %s\nbest score = %f\n" % (gs.best_params_, gs.best_score_))
-
-    t2 = time.clock()
-    print('time:', t2-t1)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--train', default='train.tsv', help='Training data in TSV format')
+    parser.add_argument('-v', '--verbose', type=int, default=2, help='Verbosity')
 
     args = parser.parse_args()
     main(args)
