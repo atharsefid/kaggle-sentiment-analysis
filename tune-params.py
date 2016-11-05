29b10cef5c9c7dc98e88564e2a266ef167030067
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-11-05 17:24:21 -0400

    tune-params.py: make prediction
    I wanted to save the model and load the model in another predict script,
    but got TypeError. It seems to be a bug in numpy.
    
    So just do the prediction at the same time.

--- tune-params.py
+++ tune-params.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Find the best estimator
+Find the best parameters for estimator pipeline and classify the sentiment of phrases
 """
 
 import argparse
-import pickle
+import csv
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
@@ -14,8 +14,8 @@ from sentiment_analysis import transforms
 
 
 def main(args):
-    X = data.load_data_points(args.train)
-    y = [x.Sentiment for x in X]
+    X_train = data.load_data_points(args.train)
+    y_train = [x.Sentiment for x in X_train]
 
     print("setup pipeline: %s..." % args.steps)
     steps = []
@@ -28,22 +28,33 @@ def main(args):
     pipeline = Pipeline(steps)
 
     print('grid search...')
-    gs = GridSearchCV(estimator=pipeline, param_grid=param_grid, cv=5, verbose=args.verbose).fit(X, y)
+    gs = GridSearchCV(estimator=pipeline, param_grid=param_grid, cv=5, verbose=args.verbose).fit(X_train, y_train)
     print('best params =', gs.best_params_)
     print('best score =', gs.best_score_)
 
-    # save the model for later prediction
-    filename = '-'.join(args.steps) + '.pickle'
-    print("save model into %s..." % filename)
-    with open(filename, 'wb') as f:
-        pickle.dump(gs, f)
+    # predict on test data
+    X_test = data.load_data_points(args.test)
+    print('make predictions...')
+    y_test = gs.predict(X_test)
+
+    # output result to CSV file
+    print("outout result to %s..." % args.outfile)
+    phrase_ids = [x.PhraseId for x in X_test]
+    results = zip(phrase_ids, y_test)
+    with open(args.outfile, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['PhraseId', 'Sentiment'])
+        for r in results:
+            writer.writerow(r)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-t', '--train', default='train.tsv', help='Training data in TSV format')
-    parser.add_argument('-v', '--verbose', type=int, default=2, help='Verbosity')
+    parser.add_argument('-T', '--train', default='train.tsv', help='Training data in TSV format')
     parser.add_argument('-s', '--steps', required=True, nargs='*', help="Steps in the pipeline, e.g. ep cv dtc. Valid steps are: %s" % ', '.join(transforms.supported_transforms()))
+    parser.add_argument('-t', '--test', default='test.tsv', help='Test data in TSV format')
+    parser.add_argument('-o', '--outfile', required=True, help='Output file in CSV format')
+    parser.add_argument('-v', '--verbose', type=int, default=2, help='Verbosity')
 
     args = parser.parse_args()
     main(args)
