d2fe8c42edc5f6b04abec77a402be24c5eeef724
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-11-04 23:57:30 -0400

    tune-params.py: add --steps

---
+++ sentiment_analysis/transforms.py
@@ -0,0 +1,65 @@
+import math
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+
+supported_transforms = {
+    'cv': CountVectorizer,
+    'dtc': DecisionTreeClassifier,
+    'rfc': RandomForestClassifier,
+    'svc': SVC,
+    'gbc': GradientBoostingClassifier,
+    'lr': LogisticRegression,
+}
+
+param_grids = {
+    'dtc': [
+        {
+            'dtc__criterion': ['gini', 'entropy'],
+            'dtc__min_samples_split': [1,2,3,4],
+        },
+    ],
+    'rfc': [
+        {
+            'rfc__criterion': ['gini', 'entropy'],
+            'rfc__min_samples_split': [1,2,3,4],
+        },
+    ],
+    'svc': [
+        {
+            'svc__C': [math.pow(10, power) for power in range(-4, 5)],
+            'svc__gamma': [math.pow(2, power) for power in range(-4, 5)],
+        },
+    ],
+    'gbc': [
+        {
+            'gbc__loss': ['deviance', 'exponential'],
+            'gbc__min_samples_split': [1,2,3,4],
+        },
+    ],
+    'lr': [
+        {
+            'lr__solver': ['newton-cg','lbfgs', 'sag'],
+            'lr__C': [math.pow(10, power) for power in range(-4, 5)],
+        }, {
+            'lr__solver': ['liblinear'],
+            'lr__penalty': ['l1', 'l2'],
+            'lr__C': [math.pow(10, power) for power in range(-4, 5)],
+        },
+    ],
+}
+
+
+def get_transform_and_param_grid(name):
+    transform = supported_transforms[name]
+    if name in param_grids:
+        param_grid = param_grids[name]
+    else:
+        param_grid = []
+
+    return transform, param_grid
--- tune-params.py
+++ tune-params.py
@@ -4,37 +4,38 @@ Find the best estimator
 """
 
 import argparse
-import math
 
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC
 
-import sentiment_analysis.data as data
+from sentiment_analysis import data
+from sentiment_analysis import transforms
 
 
 def main(args):
     X, y = data.load_training_data(args.train)
-    #print(X)
-    #print(y)
+
     print('setup pipeline...')
-    steps = [('cv', CountVectorizer()), ('svc', SVC())]
+    steps = []
+    param_grid = []
+    for step in args.steps:
+        transform, pg = transforms.get_transform_and_param_grid(step)
+        steps.append((step, transform()))
+        param_grid.extend(pg)
+
     pipeline = Pipeline(steps)
 
     print('grid search...')
-    param_grid = {
-        'svc__C': [math.pow(10, power) for power in range(-4, 5)]
-    }
-
     gs = GridSearchCV(estimator=pipeline, param_grid=param_grid, cv=5, verbose=args.verbose).fit(X, y)
-    print("best params = %s\nbest score = %f\n" % (gs.best_params_, gs.best_score_))
+    print('best params =', gs.best_params_)
+    print('best score =', gs.best_score_)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--train', default='train.tsv', help='Training data in TSV format')
     parser.add_argument('-v', '--verbose', type=int, default=2, help='Verbosity')
+    parser.add_argument('-s', '--steps', required=True, nargs='*', help='Steps in the pipeline, e.g. cv svc')
 
     args = parser.parse_args()
     main(args)
